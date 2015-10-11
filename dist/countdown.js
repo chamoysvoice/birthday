621224c58b307f967167cd5bb1bbcf708077ffa5
Jesus Garza <chamoysvoice@gmail.com>
2015-10-11 18:25:20 -0500

    Here is my birthday :)

---
+++ dist/countdown.js
@@ -0,0 +1,46 @@
+var countdown = function (date, ids, callback){
+  var _second = 1000,
+      _minute = _second * 60,
+      _hour = _minute * 60,
+      _day = _hour * 24;
+
+  var date = new Date(date),
+      timer,
+
+  calculate = function(){
+    var now = new Date();
+    var remaining = date.getTime() - now.getTime();
+    var data;
+
+    if(isNaN(date)){
+      console.log("Invalid date/time");
+      return;
+    }
+
+    if(remaining <= 0){
+      callback();
+      return;
+    } else {
+      if(!timer){
+        timer = setInterval(calculate, _second);
+      }
+    }
+
+    data = {
+      'days': Math.floor(remaining / _day),
+      'hours': Math.floor((remaining % _day) / _hour ),
+      'minutes': Math.floor((remaining % _hour) / _minute),
+      'seconds': Math.floor((remaining % _minute) / _second)
+    }
+
+   if(ids.length){
+     for(x in ids){
+       var i = ids[x];
+       document.getElementById(i).innerHTML = data[i];
+     }
+   }
+  }
+
+calculate();
+
+}
